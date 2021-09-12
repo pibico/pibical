6adc6567d3c2d4937ade1461134678f7b08ace93
PibiCo <pibico.sl@gmail.com>
2021-09-12 20:06:10 +0200

    nuevo archivo:  doctype/__init__.py 	nuevo archivo:  doctype/minute/__init__.py 	nuevo archivo:  doctype/minute/minute.json 	nuevo archivo:  doctype/minute/minute.py 	modificado:     workspace/pibical/pibical.json 	modificado:     ../fixtures/client_script.json

---
+++ pibical/pibical/doctype/minute/minute.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, pibico and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Minute(Document):
+	pass
